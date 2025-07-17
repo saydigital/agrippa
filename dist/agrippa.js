ddfa707b72905c638ff7c3f2bb0c4d13869474a0
Aron <winkler.aron5@gmail.com>
2025-07-17 11:15:41 +0200

    [rel] rebuild library

--- dist/agrippa.js
+++ dist/agrippa.js
@@ -519,9 +519,100 @@ async function performUpsync(workflowSlug, phases) {
 }
 
 async function upsyncLocalWorkflow(options = {}) {
-    const config = await selectWorkflow$1(options);
-    const spinner = ora('Getting auth token').start();
     await refreshToken();
+    if (options.all) {
+        return doAll(options);
+    }
+    else {
+        return doOne(options, { spin: true, prompt: true });
+    }
+}
+async function doAll(options) {
+    const spinner = ora('Computing phases').start();
+    const workflows = listLocalWorkflows();
+    const reportLines = {
+        safe: [],
+        missing: [],
+        stale: [],
+        noop: [],
+    };
+    const allPhases = {};
+    for (const wf of workflows) {
+        const phases = await getPhases(wf.id);
+        await backupWorkflow$1(wf.slug, { name: 'Pre upsync', phases });
+        const phasesWithStatus = computePhaseStatus(wf.slug, phases);
+        allPhases[wf.slug] = phasesWithStatus;
+        const safe = phasesWithStatus.filter((s) => s.status === 'safe');
+        const missing = phasesWithStatus.filter((s) => s.status === 'missing');
+        const stale = phasesWithStatus.filter((s) => s.status === 'stale');
+        const noop = phasesWithStatus.filter((s) => s.status === 'noop');
+        reportLines.safe = [
+            ...reportLines.safe,
+            ...safe.map((phase) => ({
+                name: `${wf.name} / ${phase.name}`,
+                id: phase.id,
+            })),
+        ];
+        reportLines.missing = [
+            ...reportLines.missing,
+            ...missing.map((phase) => ({
+                name: `${wf.name} / ${phase.name}`,
+                id: phase.id,
+            })),
+        ];
+        reportLines.stale = [
+            ...reportLines.stale,
+            ...stale.map((phase) => ({
+                name: `${wf.name} / ${phase.name}`,
+                id: phase.id,
+            })),
+        ];
+        reportLines.noop = [
+            ...reportLines.noop,
+            ...noop.map((phase) => ({
+                name: `${wf.name} / ${phase.name}`,
+                id: phase.id,
+            })),
+        ];
+    }
+    spinner.stop();
+    const answer = await inquirer.prompt([
+        {
+            name: 'confirm',
+            type: 'confirm',
+            message: [
+                'Summary of operations:',
+                '',
+                ...reportLines.safe.map((line) => `- [SYNC-SAFE] ${line.name}`),
+                '',
+                ...reportLines.stale.map((line) => `- [SYNC-DANGER] ${line.name}`),
+                '',
+                ...reportLines.missing.map((line) => `- [SKIP-MISSING] ${line.name}`),
+                '',
+                ...(options.reportNoop
+                    ? reportLines.missing.map((line) => `- [SKIP-MISSING] ${line.name}`)
+                    : []),
+                '',
+            ].join('\n'),
+        },
+    ]);
+    if (!answer.confirm) {
+        return;
+    }
+    spinner.text = 'Syncing';
+    spinner.start();
+    for (const wf of workflows) {
+        const phases = allPhases[wf.slug];
+        await performUpsync(wf.slug, phases);
+    }
+    spinner.stop();
+}
+async function doOne(options, { prompt, spin }) {
+    const config = await selectWorkflow$1(options);
+    const spinner = ora('Getting auth token');
+    {
+        spinner.start();
+    }
     spinner.text = 'Downloading phases';
     const phases = await getPhases(config.id);
     const phasesWithStatus = computePhaseStatus(config.slug, phases);
@@ -530,52 +621,56 @@ async function upsyncLocalWorkflow(options = {}) {
     const stale = phasesWithStatus.filter((s) => s.status === 'stale');
     const noop = phasesWithStatus.filter((s) => s.status === 'noop');
     spinner.stop();
-    const report = await inquirer.prompt([
-        {
-            name: 'confirm',
-            type: 'confirm',
-            message: [
-                'Summary of operations that will be performed:',
-                '',
-                ...(stale.length > 0
-                    ? [
-                        '[WARNING - STALE] The following phases were modified on the remote after',
-                        'your last sync, make sure you want to overwrite them:',
-                        ...stale.map((phase) => `- ${phase.name}`),
-                        '',
-                    ]
-                    : []),
-                ...(missing.length > 0
-                    ? [
-                        '[ERROR - MISSING] The following phases have ids that can ',
-                        'no longer be found on the remote, so they will not be written to Odoo:',
-                        ...missing.map((phase) => `- ${phase.name}`),
-                        '',
-                    ]
-                    : []),
-                ...(safe.length > 0
-                    ? [
-                        '[SAFE] These phases were modified and do NOT conflict with the remote',
-                        'since the last sync:',
-                        ...safe.map((phase) => `- ${phase.name}`),
-                        '',
-                    ]
-                    : []),
-                ...(noop.length > 0
-                    ? [
-                        '[NOOP] These phases are the same as on the remote:',
-                        ...noop.map((phase) => `- ${phase.name}`),
-                    ]
-                    : []),
-                'Confirm',
-            ].join('\n'),
-        },
-    ]);
-    if (!report.confirm) {
-        return;
+    {
+        const report = await inquirer.prompt([
+            {
+                name: 'confirm',
+                type: 'confirm',
+                message: [
+                    'Summary of operations that will be performed:',
+                    '',
+                    ...(stale.length > 0
+                        ? [
+                            '[WARNING - STALE] The following phases were modified on the remote after',
+                            'your last sync, make sure you want to overwrite them:',
+                            ...stale.map((phase) => `- ${phase.name}`),
+                            '',
+                        ]
+                        : []),
+                    ...(missing.length > 0
+                        ? [
+                            '[ERROR - MISSING] The following phases have ids that can ',
+                            'no longer be found on the remote, so they will not be written to Odoo:',
+                            ...missing.map((phase) => `- ${phase.name}`),
+                            '',
+                        ]
+                        : []),
+                    ...(safe.length > 0
+                        ? [
+                            '[SAFE] These phases were modified and do NOT conflict with the remote',
+                            'since the last sync:',
+                            ...safe.map((phase) => `- ${phase.name}`),
+                            '',
+                        ]
+                        : []),
+                    ...(options.reportNoop && noop.length > 0
+                        ? [
+                            '[NOOP] These phases are the same as on the remote:',
+                            ...noop.map((phase) => `- ${phase.name}`),
+                        ]
+                        : []),
+                    'Confirm',
+                ].join('\n'),
+            },
+        ]);
+        if (!report.confirm) {
+            return;
+        }
     }
     spinner.text = 'Taking a backup';
-    spinner.start();
+    {
+        spinner.start();
+    }
     await backupWorkflow$1(config.slug, { phases, name: 'Pre upsync' });
     spinner.text = 'Syncing';
     await performUpsync(config.slug, phasesWithStatus);
@@ -707,6 +802,8 @@ program.command('clone').action(() => {
 program
     .command('upsync')
     .option('-w, --workflow <slug>')
+    .option('-a, --all')
+    .option('--report-noop')
     .action((opts) => {
     if (opts.workflow && opts.workflow.endsWith('/')) {
         opts.workflow = opts.workflow.replace(/\/$/, '');
